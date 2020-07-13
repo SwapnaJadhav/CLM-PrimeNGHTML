229ecd04a2479f77c3dbb841f5900bf97f4ac37b
SwapnaJadhav <swapnajadhav396@gmail.com>
2020-07-13 19:25:43 +0530

    Outer_layout
    Data Content with layout

---
+++ MVP-UI/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { OuterLayoutComponent } from './outer-layout/outer-layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import {SidebarModule} from 'primeng/sidebar';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OuterLayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MenubarModule,
+    InputTextModule,
+    ButtonModule,
+    SidebarModule
+],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
